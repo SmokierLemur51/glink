ab36a6354f2b119e65ec6a34dfa8a8d068ad4062
SmokierLemur51 <69629938+SmokierLemur51@users.noreply.github.com>
2024-06-19 15:43:11 +0000

    testing

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import subprocess
+from pathlib import Path
 
-# with open("output/x.py", "w") as x:
-#     data = None
-#     with open("templates/python/flask/manage.py", "r") as d:
-#         data = d.read()
-#     x.write(data)
+def new_poetry_project() -> None:
+    """
+    """
+    # getting projects directory
+    home = Path.home()
+    project_directory = home / 'projects'
+    # name new projects
+    project = input("Name of new project: ")
+    poetry_package = input("Name of poetry package: ")
+    # create path to new project dir & create if not exists
+    new_project_dir = project_directory / project
+    new_project_dir.mkdir(parents=True, exist_ok=True)
+    # path to new poetry package in new_project_dir
+    new_package = new_project_dir / poetry_package
 
-package = input("Name Poetry package: ")
-subprocess.run([
-    "cd output/", 
-    "&&", 
-    "poetry new --src {}".format(package), 
-    "&&", 
-    "cd {}/src/{}".format(package, package),
-])
+    try:
+        result = subprocess.run(
+            ['poetry', 'new', '--src', new_package],
+            check=True,
+            text=True,
+            capture_output=True,    
+        )
+        print(f"Successfully created new project [{project}] with poetry package [{poetry_package}]")
+        print(f"Output: {result.stdout}")
+    except subprocess.CalledProcessError as e: 
+        print(f"Failed with error message:\n{e}")
+        print(f"Eror output: {e.stderr}")
 
+
+def install_requirements() -> None:
+    with open('requirements.txt', 'r') as f:
+        for x in f:
+            subprocess.run(['pip', 'uninstall', x.strip()], check=True)
+
+
+new_poetry_project()
